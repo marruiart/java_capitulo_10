0a3530961ca6bf07294a9cf86f31f679d3259e91
Marina Ruiz <marruiart@gmail.com>
2023-01-21 20:19:39 +0100

    Add Ejercicio02

---
+++ Ejercicio02.java
@@ -0,0 +1,29 @@
+import java.util.ArrayList;
+import java.util.Collections;
+
+/**
+ * Realiza un programa que introduzca valores aleatorios (entre 0 y 100) en un
+ * ArrayList y que luego calcule la suma, la media, el máximo y el mínimo de
+ * esos números. El tamaño de la lista también será aleatorio y podrá oscilar
+ * entre 10 y 20 elementos ambos inclusive.
+ *
+ * @author: Marina Ruiz Artacho
+ **/
+
+public class Ejercicio02 {
+    public static void main(String[] args) {
+        ArrayList<Integer> nums = new ArrayList<Integer>();
+        int size = (int) (Math.random() * ((20 - 10) + 1)) + 10;
+        long suma = 0;
+        double media;
+
+        for (int i = 0; i < size; i++) {
+            int n = (int) (Math.random() * 101);
+            nums.add(n);
+            suma += n;
+        }
+        media = suma / nums.size();
+        Collections.sort(nums);
+        System.out.printf("Suma: %d\nMedia: %.2f\nMáximo: %d\nMínimo: %d\n", suma, media, nums.get(0), nums.get(nums.size() - 1));
+    }
+}
