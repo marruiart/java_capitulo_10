b07d437a0986178fbe128e1ee0c57f7caed849bc
Marina Ruiz <marruiart@gmail.com>
2023-01-23 11:12:58 +0100

    Update Ejercicio10

---
+++ classes/Diccionario.java
@@ -0,0 +1,29 @@
+package classes;
+
+import java.util.HashMap;
+
+public class Diccionario {
+    public static HashMap<String, String> entradas = new HashMap<String, String>();
+    static {
+        entradas.put("hola", "hello");
+        entradas.put("adiós", "goodbye");
+        entradas.put("por favor", "please");
+        entradas.put("gracias", "thank you");
+        entradas.put("nombre", "name");
+        entradas.put("edad", "age");
+        entradas.put("bien", "good");
+        entradas.put("mal", "bad");
+        entradas.put("agua", "water");
+        entradas.put("comida", "food");
+        entradas.put("tienda", "store");
+        entradas.put("dinero", "money");
+        entradas.put("autobús", "bus");
+        entradas.put("coche", "car");
+        entradas.put("bicicleta", "bicycle");
+        entradas.put("teléfono", "telephone");
+        entradas.put("hospital", "hospital");
+        entradas.put("policía", "police");
+        entradas.put("pasaporte", "passport");
+        entradas.put("ordenador", "computer");
+    }
+}
